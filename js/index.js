b1f18e6584c39fa869f11137cf052361a510477f
Yuxin <lxfly2000@outlook.com>
2023-12-29 13:46:52 +0800

    Update.

--- js/index.js
+++ js/index.js
@@ -17,15 +17,17 @@ function LoadSettings(){
     var m=location.search.match(/se=(.*)&?/);
     //Cookie是一种用等号和分号记录的字符串数据
     //[key]=[value](;[key]=[value];...)
-    if(m==null){
-        m=document.cookie.match(/se=(.*);?/);
-    }
     var doms=document.getElementsByName("se");
-    if(m==null){
-        doms[0].checked=true;
-        return;
+    var se="";
+    if(m!=null){
+        se=decodeURIComponent(m[1]);
+    }else{
+        se=localStorage.getItem("se");
+        if(se==null){
+            doms[0].checked=true;
+            return;
+        }
     }
-    var se=m[1];
     for(var i=0;i<doms.length;i++){
         doms[i].checked=doms[i].value===se;
     }
@@ -33,53 +35,52 @@ function LoadSettings(){
 
 function LoadHistory(){
     //检查是否有记录
-    var m=document.cookie.match(/qh=(.*);?/);
+    var m=localStorage.getItem("qh");
     //有则显示无则隐藏
     if(m==null){
         divHistory.style.display="none";
     }else{
         divHistory.style.display="";
-        var entries=m[1].split(",");
+        var entries=m.split(",");
         for(var i=0;i<entries.length;i++){
             if(i==0){
                 linksHistory.innerHTML=" ";
             }else{
                 linksHistory.innerHTML+=" ";
             }
-            linksHistory.innerHTML+="<a href='javascript:SearchKeywords(\""+decodeURIComponent(entries[i])+"\")'>"+decodeURIComponent(entries[i])+"</a>";
+            linksHistory.innerHTML+="<a href='javascript:textQuery.value=\""+decodeURIComponent(entries[i])+"\";GoSearch();'>"+decodeURIComponent(entries[i])+"</a>";
         }
     }
 }
 
 //保存设置
 function SaveSettings(se){
-    document.cookie.replace(/se=(.*);?/,"");
-    var entry="se="+se;
-    if(document.cookie.length>0&&!document.cookie.endsWith(";")){
-        document.cookie=document.cookie+";";
-    }
-    document.cookie=document.cookie+entry;
+    localStorage.setItem("se",se);
 }
 
 function AddHistory(q){
     var encoded_query=encodeURIComponent(q);
     var histories=[];
-    var match_histories=document.cookie.match(/qh=(.*);?/);
+    var match_histories=localStorage.getItem("qh");
     if(match_histories!=null){
-        histories=match_histories[1].split(",");
+        histories=match_histories.split(",");
+    }
+    var pos=histories.indexOf(encoded_query);
+    if(pos!==-1){
+        histories.splice(pos,1);
     }
     //加入一个记录
     histories.splice(0,0,encoded_query);
-    document.cookie.replace(/qh=(.*);?/,"");
-    document.cookie=document.cookie+"qh="+histories[0];
+    var qh=histories[0];
     for(var i=1;i<Math.min(histories.length,10);i++){
-        document.cookie=document.cookie+","+histories[i];
+        qh=qh+","+histories[i];
     }
+    localStorage.setItem("qh",qh);
     LoadHistory();
 }
 
 function ClearHistory(){
-    document.cookie.replace(/qh=(.*);?/,"");
+    localStorage.removeItem("qh");
     LoadHistory();
 }
 
@@ -99,10 +100,4 @@ function GoSearch(){
     SaveSettings(se);
     AddHistory(textQuery.value);
     location.href=se.replace("{query}",encodeURIComponent(textQuery.value));
-}
-
-function SearchKeywords(k){
-    var se=GetSearchEngine();
-    SaveSettings(se);
-    location.href=se.replace("{query}",encodeURIComponent(k));
 }
