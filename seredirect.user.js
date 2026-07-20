117396c18c2a1df081b12735d69079c91bcc874f
Yuxin <lxfly2000@outlook.com>
2026-07-20 11:06:39 +0800

    Add booru sites

--- seredirect.user.js
+++ seredirect.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         使用我的搜索引擎
 // @namespace    https://github.com/lxfly2000/secollection/raw/master/seredirect.user.js
-// @version      0.1
+// @version      0.2
 // @updateURL    https://github.com/lxfly2000/secollection/raw/master/seredirect.user.js
 // @downloadURL  https://github.com/lxfly2000/secollection/raw/master/seredirect.user.js
 // @description  将常见的搜索引擎主页跳转到我的搜索引擎页面
@@ -9,6 +9,8 @@
 // @match        *://*.bing.com/
 // @match        *://www.baidu.com/
 // @match        *://www.bilibili.com/
+// @match        *://safebooru.donmai.us/
+// @match        *://konachan.net/
 // @icon         https://avatars0.githubusercontent.com/u/11847274
 // @grant        none
 // ==/UserScript==
@@ -19,7 +21,9 @@
     var redirect={
         "bing":"https://lxfly2000.github.io/secollection/?se=https://bing.com/search?q={query}",
         "baidu":"https://lxfly2000.github.io/secollection/?se=https://www.baidu.com/s?wd={query}",
-        "bilibili":"https://lxfly2000.github.io/secollection/?se=https://search.bilibili.com/all?keyword={query}"
+        "bilibili":"https://lxfly2000.github.io/secollection/?se=https://search.bilibili.com/all?keyword={query}",
+        "safebooru.donmai.us":"https://danbooru.donmai.us",
+        "konachan.net":"https://konachan.com"
     };
     for(var k of Object.keys(redirect)){
         if(location.host.indexOf(k)!==-1){
